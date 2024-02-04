fba481c3f0691f85141a82c105d99be1212b120b
henrybenso <henrykbenso1@gmail.com>
2024-02-03 19:38:37 -0800

    lecture 01-31 code-along

---
+++ test/compiler.test.js
@@ -0,0 +1,7 @@
+import assert from "assert/strict"
+
+describe('Equality Test', () => {
+    it('should return true when 1 is equal to 1', () => {
+        assert.equal(1 === 1, true);
+    });
+});
